6777641be51a68eb27a9af00f9d3c770132143c3
maciek@mint <mcyran1996@gmail.com>
2023-08-02 19:19:56 +0200

    lesson 55

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {RecipeModel} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: RecipeModel[] = [
+    new RecipeModel('A test recipe', 'Test desc', 'https://ps.w.org/wp-recipe-maker/assets/icon-128x128.png?rev=1491788')
+  ];
+}
