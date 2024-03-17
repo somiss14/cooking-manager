54471bb03d07cd25edda261270d11e4ed85ac8ff
maciek@arch <mcyran1996@gmail.com>
2024-03-18 00:10:14 +0100

    lesson 297

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,33 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.comonent.html'
 })
-export class AuthComponent{
+export class AuthComponent implements OnInit{
   isLoginMode = true;
+  authForm: FormGroup;
+
+  ngOnInit() {
+    this.initForm();
+  }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  private initForm() {
+    let email = '';
+    let password = '';
+    this.authForm = new FormGroup({
+      'email': new FormControl(email, [Validators.required, Validators.email]),
+      'password': new FormControl(password, [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log(form.value);
+    form.reset();
   }
 }
