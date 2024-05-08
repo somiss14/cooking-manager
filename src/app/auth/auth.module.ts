86aaea470646f6d5ed131b43a944f4f738a0c330
maciek@mint <mcyran1996@gmail.com>
2024-05-08 21:14:12 +0200

    lesson 334

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AuthComponent} from "./auth.component";
+import {SharedModule} from "../shared/shared.module";
+import {RouterModule} from "@angular/router";
+
+
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    RouterModule.forChild([{path: 'auth', component: AuthComponent}])
+  ]
+})
+export class AuthModule { }
